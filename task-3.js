8ff35178dd058c4d685e64361af59b5686fb5da4
Yakiv Shypelyk <schypelyk128@ukr.net>
2020-11-14 23:54:08 +0200

    пофіксив 3ю задачу

--- task-3.js
+++ task-3.js
@@ -33,7 +33,7 @@ const makeTransaction = transaction => {
     });
   };
   
-  const logSuccess = (id, time) => {
+  const logSuccess = ({id, time}) => {
     console.log(`Transaction ${id} processed in ${time}ms`);
   };
   
